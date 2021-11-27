e97bf51971a0427ce5875fdec5f7d541202d4c17
vijayapalponram <vijayapal.p@gmail.com>
2021-11-27 23:16:36 +0530

    Add project files.

---
+++ NetCoreAPILearning/Controllers/LeaveController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreAPILearning.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class LeaveController : ControllerBase
+    {
+        [HttpGet]
+        public string GetLeaves()
+        {
+            return "Hello world1";
+        }
+
+        [HttpGet]
+        [Route("single")]
+        public string GetLeaveForone()
+        {
+            return "Hello world1";
+        }
+        [HttpPost]
+        public string CreateLeave()
+        {
+            return "Hello world2";
+        }
+        [HttpDelete]
+        public string DeleteLeave()
+        {
+            return "Hello world1";
+        }
+
+        [HttpPut]
+        public string UpdateLeave()
+        {
+            return "Hello world2";
+        }
+    }
+}
